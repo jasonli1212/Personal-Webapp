0717c478c9c35ed0b37fc89c9bc91878090d9a7f
jasonli1212 <lijason166@gmail.com>
2025-07-13 14:13:54 -0700

    Added navBar background

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -2,11 +2,15 @@ import React from 'react'
 import styled from "styled-components";
 
 const Section = styled.div`
+  width: 100%;
   z-index: 999;
   display: flex;
   justify-content: center;
   align-items: center;
   font-size: 1.2rem;
+  background-image: linear-gradient(to bottom right, rgba(255,255,255,0.3), rgba(255,255,255,0.2));
+  box-shadow: 0px 0px 2px rgba(30,30,30,0.5);
+  backdrop-filter: blur(10px);
 
   @media only screen and (max-width: 768px) {
     width: 100%;
@@ -17,7 +21,7 @@ const Section = styled.div`
 const Container = styled.div`
   width: 100%;
   display: flex;
-  justify-content: space-between;
+  justify-content: center;
   align-items: center;
   padding: 10px 0px;
 
